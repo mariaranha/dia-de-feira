8240cc7be035259edafa8719b5be28b4acfe6458
Marina Miranda Aranha <ra174233@students.ic.unicamp.br>
2020-01-13 10:01:28 -0300

    add swift project

---
+++ DiaDeFeira/DiaDeFeira/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiaDeFeira
+//
+//  Created by Marina Miranda Aranha on 13/01/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
