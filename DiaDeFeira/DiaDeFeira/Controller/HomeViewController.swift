9b355a1da2f813bd80f6a88d8c04edbb17076bc3
Marina Miranda Aranha <ra174233@students.ic.unicamp.br>
2020-01-21 11:34:16 -0300

    add user location on map

---
+++ DiaDeFeira/DiaDeFeira/Controller/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  DiaDeFeira
+//
+//  Created by Marina Miranda Aranha on 13/01/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    fileprivate let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        checkLocationPermission()
+        
+        locationManager.delegate = self
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       let location = locations.first!
+       let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 500, longitudinalMeters: 500)
+       mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    //MARK: - Permissions
+    func checkLocationPermission() {
+      if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+         locationManager.startUpdatingLocation()
+         self.mapView.showsUserLocation = true
+      } else {
+         locationManager.requestWhenInUseAuthorization()
+      }
+    }
+
+}
+
