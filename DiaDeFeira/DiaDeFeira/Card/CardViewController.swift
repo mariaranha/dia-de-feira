51f5b97981e3c68f08fdc8ae21c8a2a27ef8ec45
Julia Mestre <juliacmestre@gmail.com>
2020-08-19 09:15:55 -0300

    Added market card xib

---
+++ DiaDeFeira/DiaDeFeira/Card/CardViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CardViewController.swift
+//  DiaDeFeira
+//
+//  Created by Julia Conti Mestre on 17/08/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var handleArea: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
