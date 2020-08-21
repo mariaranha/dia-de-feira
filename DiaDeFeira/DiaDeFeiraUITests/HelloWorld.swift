30b7cf24a4980d8afac1f144422433ae4973a20d
Matheus de Souza Oliveira <m203407@dac.unicamp.br>
2020-08-21 11:15:25 -0300

    Testing new workflow

---
+++ DiaDeFeira/DiaDeFeiraUITests/HelloWorld.swift
@@ -0,0 +1,18 @@
+//
+//  HelloWorld.swift
+//  DiaDeFeiraUITests
+//
+//  Created by Matheus Oliveira on 21/08/20.
+//  Copyright © 2020 Marina Miranda Aranha. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import DiaDeFeira
+
+class HelloWorld: XCTestCase {
+    
+    func testHelloWorld() {
+        XCTAssert(true, "Hello World")
+    }
+}
